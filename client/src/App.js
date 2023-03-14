1da9ef5c2705fc1fecdc13d5839552c21b64ae26
Crazynuget <andydchoo@gmail.com>
2023-03-13 20:31:01 -0400

    adding hhttps?

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-const api_base = 'dark-lime-xerus-tux.cyclic.app';
+const api_base = 'https://dark-lime-xerus-tux.cyclic.app/';
 
 function App() {
   const [todos, setTodos] = useState([]);
